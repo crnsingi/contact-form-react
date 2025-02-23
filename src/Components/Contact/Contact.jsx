c7631260dc6fcedcf75dd1aa4d762fc95a867212
Cesar <cesarnsingi@hotmail.com>
2025-02-23 22:20:00 +0200

    div input box email address

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,8 +8,13 @@ const Contact = () => {
             <h2>Contact Form</h2>
             <div className="input-box">
                 <label>Full Name</label>
-                <input type="text" className="field"/>
+                <input type="text" className="field" placeholder='Enter your name' required/>
             </div>
+            <div className="input-box">
+                <label>Email Address</label>
+                <input type="email" className="field" placeholder='Enter your email' required/>
+                </div>
+            
         </form>
     </section>
   )
